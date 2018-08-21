b878225f111ce6a59b2a2832eca7e745eed9958c
Rubén Romero <romeroa.ruben@gmail.com>
2018-08-21 13:37:04 -0300

    Práctico semana 03

---
+++ semana_03_practico/C#_TPS_InicioSesion/TPS_InicioSesion/TPS_InicioSesion/DBHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+public class DBHelper
+{
+
+    private string string_conexion = "Data Source=.\\sqlexpress;Initial Catalog=DB_Bugs;Integrated Security=True";
+
+
+    public DataTable ConsultaSQL(string strSql)
+    {
+        //  Se utiliza para sentencias SQL del tipo Select
+        //  La funci�n recibe por valor una sentencia sql como string, devuelve un objeto de tipo DataTable
+        SqlConnection conexion = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        DataTable tabla = new DataTable();
+        try
+        {
+            conexion.ConnectionString = string_conexion;
+            conexion.Open();
+            cmd.Connection = conexion;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = strSql;
+            //  El datatable se carga con el resultado de ejecutar la sentencia en el motor de base de datos
+
+            tabla.Load (cmd.ExecuteReader());
+            //  La funci�n retorna el objeto datatable con 0, 1 o mas registros
+            return tabla;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        finally
+        {
+            if ((conexion.State == ConnectionState.Open))
+            {
+                conexion.Close();
+            }
+
+            // Dispose() libera los recursos asociados a la conex�n
+            conexion.Dispose();
+        }
+
+    }
+}
