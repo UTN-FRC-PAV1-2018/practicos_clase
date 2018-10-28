9ac7c2bae2dbe72b26328aaa12823a9d9e18ac3a
Rubén Romero <romeroa.ruben@gmail.com>
2018-10-28 18:42:06 -0300

    Modificaciones alta de Bug + pasar a testing + cerrar bug

---
+++ semana_12_transaccion_bug/VB_TPS_BugTracker/TPS_BugTracker/BusinessLayer/Services/BugService.vb
@@ -0,0 +1,44 @@
+﻿
+Public Class BugService
+    Private oBugDao As BugDao
+    Private oHistorialBugDao As HistorialBugDao
+
+    Public Sub New()
+        oBugDao = New BugDao()
+        oHistorialBugDao = New HistorialBugDao()
+    End Sub
+
+    Public Function consultarBugsConFiltros(ByVal params As List(Of Object)) As IList(Of Bug)
+        Return oBugDao.getBugByFilters(params)
+    End Function
+
+    Public Function consultarBugsPorId(ByVal id As Integer) As Bug
+        Return oBugDao.getBugById(id)
+    End Function
+
+    Public Function actualizarBug(ByVal oBug As Bug) As Boolean
+        Return oBugDao.update(oBug)
+    End Function
+
+    Public Function crearBug(ByVal oBug As Bug) As Boolean
+        Return oBugDao.create(oBug)
+    End Function
+
+    Public Function pasarATesting(ByVal oBug As Bug) As Boolean
+        Dim historial As HistorialBug = New HistorialBug()
+        historial.responsable = frmPrincipal.obtenerUsuarioLogin().id_usuario
+        historial.estado = 4
+        historial.fecha = DateTime.Now
+        oBug.addHistorial(historial)
+        Return oBugDao.update(oBug)
+    End Function
+
+    Public Function cerrar(ByVal oBug As Bug) As Boolean
+        Dim historial As HistorialBug = New HistorialBug()
+        historial.responsable = frmPrincipal.obtenerUsuarioLogin().id_usuario
+        historial.estado = 3
+        historial.fecha = DateTime.Now
+        oBug.addHistorial(historial)
+        Return oBugDao.update(oBug)
+    End Function
+End Class
---
+++ semana_12_transaccion_bug/VB_TPS_BugTracker/TPS_BugTracker/DataLayer/Helper/BDHelper.vb
@@ -0,0 +1,103 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class BDHelper
+    Private string_conexion As String = "Data Source=maquis;Initial Catalog=DB_Bugs;User id=avisuales1;Password=avisuales1;"
+    Private Shared instance As BDHelper
+
+    Public Shared Function getBDHelper() As BDHelper
+        If instance Is Nothing Then instance = New BDHelper()
+        Return instance
+    End Function
+
+    Public Function ConsultaSQL(ByVal strSql As String) As DataTable
+        Dim conexion As SqlConnection = New SqlConnection()
+        Dim cmd As SqlCommand = New SqlCommand()
+        Dim tabla As DataTable = New DataTable()
+
+        Try
+            conexion.ConnectionString = string_conexion
+            conexion.Open()
+            cmd.Connection = conexion
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = strSql
+            tabla.Load(cmd.ExecuteReader())
+            Return tabla
+        Catch ex As Exception
+            Throw ex
+        Finally
+
+            If (conexion.State = ConnectionState.Open) Then
+                conexion.Close()
+            End If
+
+            conexion.Dispose()
+        End Try
+    End Function
+
+    Public Function ConsultaSQLConParametros(ByVal strSql As String, ByVal sqlParam As List(Of Object)) As DataTable
+        Dim conexion As SqlConnection = New SqlConnection()
+        Dim cmd As SqlCommand = New SqlCommand()
+        Dim tabla As DataTable = New DataTable()
+
+        Try
+            conexion.ConnectionString = string_conexion
+            conexion.Open()
+            cmd.Connection = conexion
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = strSql
+            Dim indice = 0
+
+            For Each param In sqlParam
+
+                If param IsNot Nothing Then
+                    Dim n_param = "param" & Convert.ToString(indice + 1)
+                    cmd.Parameters.AddWithValue(n_param, param)
+                End If
+
+                indice += 1
+            Next
+
+            tabla.Load(cmd.ExecuteReader())
+            Return tabla
+        Catch ex As Exception
+            Throw ex
+        Finally
+
+            If (conexion.State = ConnectionState.Open) Then
+                conexion.Close()
+            End If
+
+            conexion.Dispose()
+        End Try
+    End Function
+
+    Public Function EjecutarSQL(ByVal strSql As String, Optional ByVal parametros As List(Of SqlParameter) = Nothing) As Integer
+        Dim conexion As SqlConnection = New SqlConnection()
+        Dim cmd As SqlCommand = New SqlCommand()
+        Dim t As SqlTransaction = Nothing
+        Dim rtdo As Integer = 0
+
+        Try
+            conexion.ConnectionString = string_conexion
+            conexion.Open()
+            t = conexion.BeginTransaction()
+            cmd.Connection = conexion
+            cmd.Transaction = t
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = strSql
+            If parametros IsNot Nothing Then cmd.Parameters.AddRange(parametros.ToArray())
+            rtdo = cmd.ExecuteNonQuery()
+            t.Commit()
+        Catch ex As Exception
+            If t IsNot Nothing Then t.Rollback()
+        Finally
+            If conexion.State = ConnectionState.Open Then conexion.Close()
+            conexion.Dispose()
+        End Try
+
+        Return rtdo
+    End Function
+End Class
